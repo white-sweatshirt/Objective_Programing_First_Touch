9ec09e65f0b72b9a8270c75ac82c3fc7272c34f9
white_sweatshirt <wawer.franciszek@gmail.com>
2025-11-06 20:22:17 +0100

    removed uncessery output

--- out/build/ad/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/franc/OneDrive/Pulpit/Code on commsion or homework/PO_Pd_2_1
-# Build directory: C:/Users/franc/OneDrive/Pulpit/Code on commsion or homework/PO_Pd_2_1/out/build/ad
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("animals")
-subdirs("people")
