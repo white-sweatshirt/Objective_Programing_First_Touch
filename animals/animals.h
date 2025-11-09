a6cd147262e7f72df854d3c0c976a86bf50d7a4c
white_sweatshirt <wawer.franciszek@gmail.com>
2025-11-09 18:35:51 +0100

    removing unmecessy folder

---
+++ animals/animals.h
@@ -0,0 +1,24 @@
+#ifndef ANIMALS_H
+#define ANIMALS_H
+#include "people.h"
+using namespace std;
+
+class Animal
+{
+protected:
+    friend Customer;
+    bool stomAche;
+    string name;
+
+public:
+    virtual void giveVoice();
+    virtual void healAnimal
+};
+class Dog
+{
+
+    bool illPawn;
+bool public : void healAnimal();
+    void giveVoice();
+};
+#endif
