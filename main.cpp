69015d596e2c946d03150b2472103973676fdebc
white_sweatshirt <wawer.franciszek@gmail.com>
2025-11-09 20:33:30 +0100

    Revert "Resolving problem of dependecis . ie now there are library of clients and employees"
    This reverts commit 09b42890c76ae18702d17bb11040217b48a81126.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+#include "DivisonOfBank.h"
+>>>>>>> parent of 09b4289 (Resolving problem of dependecis . ie now there are library of clients and employees)
 
 
 int main(void)
