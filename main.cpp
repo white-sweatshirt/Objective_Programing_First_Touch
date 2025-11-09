092e2223d149ce89f05c19f29eb4fb3a6208bd2b
white_sweatshirt <wawer.franciszek@gmail.com>
2025-11-09 20:34:21 +0100

    Revert "gfd"
    This reverts commit ca54c8c212173fba06acf8060ef43585082d1f41.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 #include "DivisonOfBank.h"
 >>>>>>> parent of 09b4289 (Resolving problem of dependecis . ie now there are library of clients and employees)
 
+=======
+#include "employees.h"
+>>>>>>> parent of ca54c8c (gfd)
 
 int main(void)
 {
