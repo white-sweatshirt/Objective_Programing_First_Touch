3c73ead30bb879048ea59275ddaf944f45d484de
white_sweatshirt <wawer.franciszek@gmail.com>
2025-11-09 20:38:16 +0100

    Revert "Resolving problem of dependecis . ie now there are library of clients and employees"
    This reverts commit 09b42890c76ae18702d17bb11040217b48a81126.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 #include "DivisonOfBank.h"
->>>>>>> parent of 09b4289 (Resolving problem of dependecis . ie now there are library of clients and employees)
 
-=======
-#include "employees.h"
->>>>>>> parent of ca54c8c (gfd)
-=======
-#include "employees.h"
->>>>>>> parent of ca54c8c (gfd)
+void checkAllMethodsInDivision(DivisonOfBank &unit)
+{
+    // meant for checking if all methods work correctly and wheter there is sth to repair
+    // fuction so that I wont have to wirte 2 times the same checks...
+    // assumes that unit is refence to class of DivisonOfBank
+    // there is no direct checking of isIt positive in this fuction only indriect by calling other
+    cout << "przed zmianami: " << endl;
+    unit.showAllInfo();
 
+    unit.changeBoss("Obleix");
+    unit.changeNumberOfDivsion(2);
+
+    unit.hirePeople(1000);
+    unit.hirePeople(-0.1);
+    unit.hirePeople(-1);
+    unit.layOff(10);
+    unit.layOff(-1);
+
+    unit.depositFunds(1090);
+    unit.depositFunds(-123);
+
+    cout << "------------------" << endl
+         << "po zmianach: " << endl;
+    unit.showAllInfo();
+}
 int main(void)
 {
 
