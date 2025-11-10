1a23e9daf379f1eddfa68e3eef418737427880f4
white_sweatshirt <wawer.franciszek@gmail.com>
2025-11-10 18:26:19 +0100

    removing unecessery output file

--- Po_2_1/out/veterinary/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/franc/OneDrive/Pulpit/gitExperiment
-# Build directory: C:/Users/franc/OneDrive/Pulpit/gitExperiment/out/veterinary
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("livingOrganism")
-subdirs("clients")
-subdirs("employes")
