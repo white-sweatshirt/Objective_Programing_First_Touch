76ae6dd0758d9cb7439da849b4b797ad507a3df0
white_sweatshirt <wawer.franciszek@gmail.com>
2025-11-10 17:12:19 +0100

    changeOfFolder

---
+++ Po_2_1/out/veterinary/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/franc/OneDrive/Pulpit/gitExperiment
+# Build directory: C:/Users/franc/OneDrive/Pulpit/gitExperiment/out/veterinary
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("livingOrganism")
+subdirs("clients")
+subdirs("employes")
