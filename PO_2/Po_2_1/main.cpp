2e46a201263f6c1ec3efd052749fd7f58d693752
white_sweatshirt <wawer.franciszek@gmail.com>
2025-11-20 20:05:36 +0100

    new age on the hill ondrive chellenged

---
+++ PO_2/Po_2_1/main.cpp
@@ -0,0 +1,32 @@
+#include "clients.h"
+#include "employes.h"
+void sperateConversions(void)
+{
+    int constexpr numberOfSeperators = 20;
+    for (int i = 0; i < numberOfSeperators; i++)
+        cout << "=";
+    cout << endl;
+}
+int main(void)
+{
+    Customer* ala=new Customer(new Dog("Pimpek"), 1000, "Ala", 21);
+    Vet *flitz;
+    Customer *basa = new Customer(new Parrot("Jack Sparoow"), 100, "Basa", 30);
+    flitz = new Vet("Fryderyk", 34, "masters");
+    Vet *filp=new Vet("Filip",23,"masters");
+    flitz->introduceYourself();
+
+    ala->askForHelpWithAnimal(flitz);
+    sperateConversions();
+
+    basa->askForHelpWithAnimal(flitz);
+    sperateConversions();
+
+    ala->askForHelpWithAnimal(flitz);
+    sperateConversions();
+    basa->askForHelpWithAnimal(filp);
+    delete ala;
+    delete basa;
+    flitz->sayHowMuchHaveYouMade();
+    filp->sayHowMuchHaveYouMade();
+}
--- Po_2_1/livingOrganism/person/person.cpp
+++
@@ -1,6 +0,0 @@
-#include "person.h"
-void Person::introduceYourself()
-{
-    cout << "nazywam sie :" << this->name << endl;
-    cout << "mam " << this->age << " lat" << endl;
-}
