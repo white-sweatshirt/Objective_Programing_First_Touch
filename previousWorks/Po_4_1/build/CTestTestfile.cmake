2379413cd0efb9aeab71abf7e23f6299be1eedff
white_sweatshirt <wawer.franciszek@gmail.com>
2026-01-04 15:52:27 +0100

    deleted zips

--- previousWorks/Po_4_1/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/franc/OneDrive/Pulpit/PO_PD_GIT
-# Build directory: C:/Users/franc/OneDrive/Pulpit/PO_PD_GIT/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
